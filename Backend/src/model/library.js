7ddbb29f32e302aef197285e0d33cbf27688413c
Diwanshu <diwansu531@gmail.com>
2024-03-05 16:51:21 +0530

    CRUD

---
+++ Backend/src/model/library.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    bookName: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    publishedYear: {
+      type: Number,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false }
+);
+
+const createBookModel = (collectionName) => {
+  return mongoose.model(collectionName, bookSchema);
+};
+
+module.exports = { createBookModel };
