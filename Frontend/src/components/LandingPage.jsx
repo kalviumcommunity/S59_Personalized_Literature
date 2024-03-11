d76aa7d46d2dbf44fe8df7dd61922b0cc3744cad
Diwanshu <diwansu531@gmail.com>
2024-03-11 14:42:08 +0530

    Dummy data Page

--- Frontend/src/components/LandingPage.jsx
+++ Frontend/src/components/LandingPage.jsx
@@ -1,27 +1,25 @@
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+import LandingImage from "../assets/landingImage.jpg";
 
-import './LandingPage.css';
-import LandingImage from '../assets/landingImage.jpg';
-
-function LandingPage(){
-    return (
-      <div className="landingDiv">
-     
-
-        <div className="container">
-          <img src={LandingImage} alt="LandingImage" className="LandingImage" />
-          <div id='welcome'>
-            <h1>Wisdom Nexus</h1>
-            <p>
-              &quot;If you don&apos;t like to read, you haven&apos;t found the right book.&quot; -
-              J.K. Rowling </p>
-          </div>
+function LandingPage() {
+  return (
+    <div className="landingDiv">
+      <div className="container">
+        <img src={LandingImage} alt="LandingImage" className="LandingImage" />
+        <div id="welcome">
+          <h1>Wisdom Nexus</h1>
+          <p>
+            &quot;If you don&apos;t like to read, you haven&apos;t found the
+            right book.&quot; - J.K. Rowling{" "}
+          </p>
         </div>
-        <button className='landingPageButton'>Library</button>
       </div>
-    );
-    
-
+      <Link to={"/library"} className="libraryLink">
+        <button className="landingPageButton">Library</button>
+      </Link>
+    </div>
+  );
 }
 
 export default LandingPage;
-
