41e360b507ef06a56c824de3d5391c97558010d9
Diwanshu <diwansu531@gmail.com>
2024-04-25 10:19:04 +0530

    ignored files in git ignore

--- Frontend/src/components/LandingPage.jsx
+++ Frontend/src/components/LandingPage.jsx
@@ -1,27 +1,25 @@
-import Navbar from './Navbar.jsx';
-import './LandingPage.css';
-import LandingImage from '../assets/landingImage.jpg';
+import Navbar from "./Navbar.jsx";
+import "./LandingPage.css";
+import LandingImage from "../assets/landingImage.jpg";
 
-function LandingPage(){
-    return (
-      <div className="landingDiv">
-        <Navbar />
+function LandingPage() {
+  return (
+    <div className="landingDiv">
+      <Navbar />
 
-        <div className="container">
-          <img src={LandingImage} alt="LandingImage" className="LandingImage" />
-          <div id='welcome'>
-            <h1>Wisdom Nexus</h1>
-            <p>
-              &quot;If you don&apos;t like to read, you haven&apos;t found the right book.&quot; -
-              J.K. Rowling </p>
-          </div>
+      <div className="container">
+        <img src={LandingImage} alt="LandingImage" className="LandingImage" />
+        <div id="welcome">
+          <h1>Wisdom Nexus</h1>
+          <p>
+            &quot;If you don&apos;t like to read, you haven&apos;t found the
+            right book.&quot; - J.K. Rowling{" "}
+          </p>
         </div>
-        <button className='landingPageButton'>Library</button>
       </div>
-    );
-    
-
+      <button className="landingPageButton">Library</button>
+    </div>
+  );
 }
 
 export default LandingPage;
-
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import Image from '../assets/bgImg.jpeg';
-import Profile from '../assets/Profile.png';
-import './Navbar.css'; 
+import Image from "../assets/bgImg.jpeg";
+import Profile from "../assets/Profile.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-     <nav>
-        <div id="left-nav">
-            <img src={Image} alt="Image"/>
-            <p>Wisdom Nexus</p>
+  return (
+    <nav>
+      <div id="left-nav">
+        <img src={Image} alt="Image" />
+        <p>Wisdom Nexus</p>
+      </div>
+      <div id="right-nav">
+        <div className="inactive">
+          <p>Home</p>
         </div>
-        <div id="right-nav">
-            <div className="inactive">
-                <p>Home</p>
-            </div>
-            <div className="inactive"> 
-                    <p>Contacts</p>
-                   </div>
-            <div className="inactive">
-                    <p>About us</p>
-                </div>
+        <div className="inactive">
+          <p>Contacts</p>
+        </div>
+        <div className="inactive">
+          <p>About us</p>
+        </div>
 
-            <img src={Profile} alt="Profile" className='profile'/>
-        </div>
+        <img src={Profile} alt="Profile" className="profile" />
+      </div>
     </nav>
-    );
+  );
 };
 
 export default Navbar;
